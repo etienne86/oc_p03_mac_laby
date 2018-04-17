296a1225e1e6af7b9d7aa3f771ed981be2913814
etienne86 <barbier.et@gmail.com>
2018-04-17 11:05:14 +0200

    modify dashboard.py to shorten protected methods

--- dashboard.py
+++ dashboard.py
@@ -25,60 +25,30 @@ class Dashboard:
     def display(self, screen, tools=[]):
         """This method displays the dashboard.
         We assume that pygame has been initialized."""
-        if self.mode == "game":
-            self._game_display(screen, tools)
-        elif self.mode == "edit":
-            self._edit_display(screen)
-        else:
-            raise ValueError("This mode is not permitted!")
-
-    def _game_display(self, screen, tools):
-        """This method displays the dashboard in game mode.
-        We assume that pygame has been initialized."""
         side = Interface.SPRITE_SIZE
         # Our fonts
-        font_arial_16_bold = pygame.font.SysFont('Arial', 16, bold=True)
-        font_arial_14 = pygame.font.SysFont('Arial', 14)
+        font_title = pygame.font.SysFont('Arial', 16, bold=True)
+        font_txt = pygame.font.SysFont('Arial', 14)
+        fonts = (font_title, font_txt)
         # 1. First section
         # 1.1. Title
-        screen.blit(font_arial_16_bold.render('Tools found until now:',
-                                              False, (0, 0, 0)),
+        title_1 = ""
+        if self.mode == "game":
+            title_1 = "Tools found until now:"
+        elif self.mode == "edit":
+            title_1 = "Left click to select, right click to release:"
+        else:
+            raise ValueError("This mode is not permitted!")
+        screen.blit(fonts[0].render(title_1, False, (0, 0, 0)),
                     (Interface.DASHBOARD_ORIGIN[0],
                      Interface.DASHBOARD_ORIGIN[1]))
         # 1.2. Items
-        for i, tool in enumerate(tools):
-            y_tool = Interface.DASHBOARD_ORIGIN[1]\
-                    + ((2*i + (1 - 0.5*i)) * side)
-            # We draw an "empty" square
-            display_transparent_square(screen,
-                                       Interface.DASHBOARD_ORIGIN[0], y_tool)
-            # We display the tool name
-            screen.blit(font_arial_14.render(tool.name, False, (0, 0, 0)),
-                        (Interface.DASHBOARD_ORIGIN[0] + side + 10,
-                         y_tool + 10))
-            # We fill the square with the sprite if the tool is found
-            if tool.found:
-                sprite_path = os.path.join("sprites", "tools",
-                                           tool.name + ".png")
-                screen.blit(pygame.image.load(sprite_path).convert(),
-                            (Interface.DASHBOARD_ORIGIN[0], y_tool))
+        # This depends on mode (cf. _game_display and _edit_display)
         # 2. Second section
         # 2.1. Title
-        screen.blit(font_arial_16_bold.render('Status:', False, (0, 0, 0)),
+        screen.blit(fonts[0].render('Status:', False, (0, 0, 0)),
                     (Interface.DASHBOARD_ORIGIN[0],
                      Interface.DASHBOARD_ORIGIN[1] + 7 * side))
-        # 2.2. Logic light (red/yellow/green)
-        if self.logic_light == "red":
-            y_light = Interface.DASHBOARD_ORIGIN[1] + 8 * side
-            colour = (255, 0, 0)
-        elif self.logic_light == "yellow":
-            y_light = Interface.DASHBOARD_ORIGIN[1] + 9.5 * side
-            colour = (255, 242, 0)
-        elif self.logic_light == "green":
-            y_light = Interface.DASHBOARD_ORIGIN[1] + 11 * side
-            colour = (0, 255, 0)
-        else:
-            raise ValueError(self.logic_light)
         # Draw the black squares
         pygame.draw.rect(screen, (0, 0, 0),
                          (Interface.DASHBOARD_ORIGIN[0],
@@ -92,6 +62,18 @@ class Dashboard:
                          (Interface.DASHBOARD_ORIGIN[0],
                           Interface.DASHBOARD_ORIGIN[1] + 11 * side,
                           side, side))
+        # 2.2. Logic light (red/yellow/green)
+        if self.logic_light == "red":
+            y_light = Interface.DASHBOARD_ORIGIN[1] + 8 * side
+            colour = (255, 0, 0)
+        elif self.logic_light == "yellow":
+            y_light = Interface.DASHBOARD_ORIGIN[1] + 9.5 * side
+            colour = (255, 242, 0)
+        elif self.logic_light == "green":
+            y_light = Interface.DASHBOARD_ORIGIN[1] + 11 * side
+            colour = (0, 255, 0)
+        else:
+            raise ValueError(self.logic_light)
         # Draw the logic light
         pygame.draw.rect(screen, colour, (Interface.DASHBOARD_ORIGIN[0],
                                           y_light, side, side))
@@ -102,39 +84,62 @@ class Dashboard:
         display_transparent_square(screen, Interface.DASHBOARD_ORIGIN[0],
                                    Interface.DASHBOARD_ORIGIN[1] + 11 * side)
         # 2.3. Logic light description
-        screen.blit(font_arial_14.render("=> You lose... The guard kills you!",
-                                         False, (0, 0, 0)),
-                    (Interface.DASHBOARD_ORIGIN[0] + side + 10,
-                     Interface.DASHBOARD_ORIGIN[1] + 8 * side + 10))
-        screen.blit(font_arial_14.render("=> You are alive, good luck!",
-                                         False, (0, 0, 0)),
-                    (Interface.DASHBOARD_ORIGIN[0] + side + 10,
-                     Interface.DASHBOARD_ORIGIN[1] + 9.5 * side + 10))
-        screen.blit(font_arial_14.render("=> You win! Congratulations!",
-                                         False, (0, 0, 0)),
-                    (Interface.DASHBOARD_ORIGIN[0] + side + 10,
-                     Interface.DASHBOARD_ORIGIN[1] + 11 * side + 10))
+        # This depends on mode (cf. _game_display and _edit_display)
         # 3. Third section
-        screen.blit(font_arial_16_bold.render("Press 'Esc' to quit",
+        screen.blit(fonts[0].render("Press 'Esc' to quit",
                                               False, (0, 0, 0)),
                     (Interface.DASHBOARD_ORIGIN[0],
                      Interface.DASHBOARD_ORIGIN[1] + 14 * side))
+        # We call the protected methods
+        if self.mode == "game":
+            self._game_display(screen, tools, side, fonts)
+        elif self.mode == "edit":
+            self._edit_display(screen, side, fonts)
+        else:
+            raise ValueError("This mode is not permitted!")
         # Screen refresh
         pygame.display.flip()
 
-    def _edit_display(self, screen):
-        """This method displays the dashboard in edit mode.
+    def _game_display(self, screen, tools, side, fonts):
+        """This protected method displays the dashboard in game mode.
+        This method can be only called by the method "game_display".
         We assume that pygame has been initialized."""
-        side = Interface.SPRITE_SIZE
-        # Our fonts
-        font_arial_16_bold = pygame.font.SysFont('Arial', 16, bold=True)
-        font_arial_14 = pygame.font.SysFont('Arial', 14)
-        # 1. First section
-        # 1.1. Title
-        instruction = "Left click to select, right click to release:"
-        screen.blit(font_arial_16_bold.render(instruction, False, (0, 0, 0)),
-                    (Interface.DASHBOARD_ORIGIN[0],
-                     Interface.DASHBOARD_ORIGIN[1]))
+        # 1.2. Items
+        for i, tool in enumerate(tools):
+            y_tool = Interface.DASHBOARD_ORIGIN[1]\
+                    + ((2*i + (1 - 0.5*i)) * side)
+            # We draw an "empty" square
+            display_transparent_square(screen,
+                                       Interface.DASHBOARD_ORIGIN[0], y_tool)
+            # We display the tool name
+            screen.blit(fonts[1].render(tool.name, False, (0, 0, 0)),
+                        (Interface.DASHBOARD_ORIGIN[0] + side + 10,
+                         y_tool + 10))
+            # We fill the square with the sprite if the tool is found
+            if tool.found:
+                sprite_path = os.path.join("sprites", "tools",
+                                           tool.name + ".png")
+                screen.blit(pygame.image.load(sprite_path).convert(),
+                            (Interface.DASHBOARD_ORIGIN[0], y_tool))
+        # 2. Second section
+        # 2.3. Logic light description
+        screen.blit(fonts[1].render("=> You lose... The guard kills you!",
+                                         False, (0, 0, 0)),
+                    (Interface.DASHBOARD_ORIGIN[0] + side + 10,
+                     Interface.DASHBOARD_ORIGIN[1] + 8 * side + 10))
+        screen.blit(fonts[1].render("=> You are alive, good luck!",
+                                         False, (0, 0, 0)),
+                    (Interface.DASHBOARD_ORIGIN[0] + side + 10,
+                     Interface.DASHBOARD_ORIGIN[1] + 9.5 * side + 10))
+        screen.blit(fonts[1].render("=> You win! Congratulations!",
+                                         False, (0, 0, 0)),
+                    (Interface.DASHBOARD_ORIGIN[0] + side + 10,
+                     Interface.DASHBOARD_ORIGIN[1] + 11 * side + 10))
+
+    def _edit_display(self, screen, side, fonts):
+        """This protected method displays the dashboard in edit mode.
+        This method can be only called by the method "game_display".
+        We assume that pygame has been initialized."""
         # 1.2. Items
         for i in range(3):
             y_sprite = Interface.DASHBOARD_ORIGIN[1]\
@@ -154,68 +159,22 @@ class Dashboard:
             display_transparent_square(screen, Interface.DASHBOARD_ORIGIN[0],
                                        y_sprite)
             # We display the sprite name
-            screen.blit(font_arial_14.render(sprite_name, False, (0, 0, 0)),
+            screen.blit(fonts[1].render(sprite_name, False, (0, 0, 0)),
                         (Interface.DASHBOARD_ORIGIN[0] + side + 10,
                          y_sprite + 10))
-        # 2. Second section
-        # 2.1. Title
-        screen.blit(font_arial_16_bold.render('Status:', False, (0, 0, 0)),
-                    (Interface.DASHBOARD_ORIGIN[0],
-                     Interface.DASHBOARD_ORIGIN[1] + 7 * side))
-        # 2.2. Logic light (red/yellow/green)
-        if self.logic_light == "red":
-            y_light = Interface.DASHBOARD_ORIGIN[1] + 8 * side
-            colour = (255, 0, 0)
-        elif self.logic_light == "yellow":
-            y_light = Interface.DASHBOARD_ORIGIN[0] + 9.5 * side
-            colour = (255, 242, 0)
-        elif self.logic_light == "green":
-            y_light = Interface.DASHBOARD_ORIGIN[1] + 11 * side
-            colour = (0, 255, 0)
-        else:
-            raise ValueError(self.logic_light)
-        # Draw the black squares
-        pygame.draw.rect(screen, (0, 0, 0),
-                         (Interface.DASHBOARD_ORIGIN[0],
-                          Interface.DASHBOARD_ORIGIN[1] + 8 * side,
-                          side, side))
-        pygame.draw.rect(screen, (0, 0, 0),
-                         (Interface.DASHBOARD_ORIGIN[0],
-                          Interface.DASHBOARD_ORIGIN[1] + 9.5 * side,
-                          side, side))
-        pygame.draw.rect(screen, (0, 0, 0),
-                         (Interface.DASHBOARD_ORIGIN[0],
-                          Interface.DASHBOARD_ORIGIN[1] + 11 * side,
-                          side, side))
-        # Draw the logic light
-        pygame.draw.rect(screen, colour, (Interface.DASHBOARD_ORIGIN[0],
-                                          y_light, side, side))
-        display_transparent_square(screen, Interface.DASHBOARD_ORIGIN[0],
-                                   Interface.DASHBOARD_ORIGIN[1] + 8 * side)
-        display_transparent_square(screen, Interface.DASHBOARD_ORIGIN[0],
-                                   Interface.DASHBOARD_ORIGIN[0] + 9.5 * side)
-        display_transparent_square(screen, Interface.DASHBOARD_ORIGIN[0],
-                                   Interface.DASHBOARD_ORIGIN[1] + 11 * side)
         # 2.3. Logic light description
-        screen.blit(font_arial_14.render("=> This labyrinth is wrong!",
+        screen.blit(fonts[1].render("=> This labyrinth is wrong!",
                                          False, (0, 0, 0)),
                     (Interface.DASHBOARD_ORIGIN[0] + side + 10,
                      Interface.DASHBOARD_ORIGIN[1] + 8 * side + 10))
-        screen.blit(font_arial_14.render("=> This labyrinth may be playable.",
+        screen.blit(fonts[1].render("=> This labyrinth may be playable.",
                                          False, (0, 0, 0)),
                     (Interface.DASHBOARD_ORIGIN[0] + side + 10,
                      Interface.DASHBOARD_ORIGIN[0] + 9.5 * side + 10))
-        screen.blit(font_arial_14.render("=> This labyrinth is playable!",
+        screen.blit(fonts[1].render("=> This labyrinth is playable!",
                                          False, (0, 0, 0)),
                     (Interface.DASHBOARD_ORIGIN[0] + side + 10,
                      Interface.DASHBOARD_ORIGIN[1] + 11 * side + 10))
-        # 3. Third section
-        screen.blit(font_arial_16_bold.render("Press 'Esc' to quit",
-                                              False, (0, 0, 0)),
-                    (Interface.DASHBOARD_ORIGIN[0],
-                     Interface.DASHBOARD_ORIGIN[1] + 14 * side))
-        # Screen refresh
-        pygame.display.flip()
 
 
 def display_transparent_square(screen, x_pos, y_pos):
